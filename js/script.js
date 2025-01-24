497f2edfd9995838e676b791c422afd6943f1e76
marco-gulino <marcogulino98@gmail.com>
2025-01-24 21:52:49 +0100

    [feat/js] move API response into cardContainer.innerHTML

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+//OUTPUT elements of DOM
+const cardContainer = document.getElementById("container-cards");
+// const rowContainer = document.querySelectorAll(".row");
+
 //endpoint creation
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
-
-//OUTPUT elements of DOM
-const cardContainer = document.getElementById("container-main"); 
 
 //AJAX CALL WITH AXIOS LIBRARY
 axios
@@ -10,8 +11,26 @@ axios
   .get(endpoint)
   .then((respondeObj) => {
     //IF SUCCESSFUL EXECUTE THIS
-    console.log(respondeObj.data);
+    const cardsData = respondeObj.data;
 
+    // LOOP THROUGH THE ENTIRE ARRAY OF OBJECTS
+    for (let i = 0; i < cardsData.length; i++) {
+      cardContainer.innerHTML += `
+      <!-- CARD -->
+      <figure>
+        <!-- CARD PIN -->
+          <div class="red-pin"></div>
+            <!-- CARDS PHOTO CONTAINER -->
+            <div class="frame-photo">
+              <img src="${cardsData[i].url}" alt="${cardsData[i].title}" />
+            </div>
+            <!-- PHOTO INFO -->
+            <figcaption>
+              <span>${cardsData[i].date}</span>
+              <h2>${cardsData[i].title}</h2>
+            </figcaption>
+      </figure>`;
+    }
   })
   .catch((error) => {
     //IF THERE ARE ERRORS EXECUTE THIS
